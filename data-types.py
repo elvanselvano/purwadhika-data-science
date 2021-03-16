1ae9a6a4e898d2278f73cff2187f11f80d97cb51
elvanselvano <elvanselvano@yahoo.com>
2021-03-16 20:00:15 +0700

    testing

---
+++ data-types.py
@@ -0,0 +1,8 @@
+# Find Min and Max Value
+numbers = [41, 5, 1, 3, 89, 32]
+print("Minimum value = " + str(min(numbers)))
+print("Maximum value = " + str(max(numbers)))
+
+# Sort Ascending
+print("List Sebelum Di Sort = " + str(numbers))
+print("List Setelah Di Sort = " + str(sorted(numbers)))
